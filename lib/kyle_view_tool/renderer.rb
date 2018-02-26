f66a9e907ae20d888c306bcd5fca1409c5cdac77
Kyle Vermeulen <kyletvermeulen@gmail.com>
2018-02-26 21:05:12 +0000

    Updated gitignore file and readme

--- lib/kyle_view_tool/renderer.rb
+++ lib/kyle_view_tool/renderer.rb
@@ -1,6 +1,5 @@
 module KyleViewTool
 # Creating your own Gem
-module KyleViewTool
   class Renderer
     def self.copyright name, msg
       "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
@@ -8,4 +7,3 @@ module KyleViewTool
   end
 end
 #Time.now.year updates to the current year rather than hardcoding
-end
